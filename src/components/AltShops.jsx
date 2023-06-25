60f7a248345814ffa41929228505020969c1b1cc
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-06-25 22:30:45 +0100

    Component with alternative shops links ui

--- src/components/AltShops.jsx
+++ src/components/AltShops.jsx
@@ -1,16 +1,16 @@
-import img1 from '../assets/zapposPBS-_CB1509642213_.svg';
+import img1 from '../assets/black-logo.svg';
 import img2 from '../assets/vrsnl-logo-_CB1564076501_.svg'
 
 function AltShops() {
     return (
         <>
-            <div className=''>
+            <div className='flex h-15'>
 
-                <div>
-                    <img src={img1} alt="zappos" srcset="" />
+                <div className='bg-sky-600 hover:bg-sky-500 px-11 py-2 border-t-4 border-blue-900'>
+                    <img src={img1} alt="zappos" srcSet="" className='h-6 inline align-middle text-white '/>
                 </div>
-                <div>
-                <img src={img2} alt="vrsnl" srcset="" />
+                <div  className='px-10 py-2 border-r-2'>
+                <img src={img2} alt="vrsnl" srcSet="" className='h-3 inline align-middle'/>
                 </div>
             </div>
         </>
