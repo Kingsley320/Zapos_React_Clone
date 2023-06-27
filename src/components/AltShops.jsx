fafbe63a3cd356c44a3356bd55ee6ea2ca5967e1
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-06-27 17:00:35 +0100

    fixed sizing issue june 27

--- src/components/AltShops.jsx
+++ src/components/AltShops.jsx
@@ -4,12 +4,12 @@ import img2 from '../assets/vrsnl-logo-_CB1564076501_.svg'
 function AltShops() {
     return (
         <>
-            <div className='flex h-15'>
+            <div className='flex h-8'>
 
-                <div className='bg-sky-600 hover:bg-sky-500 px-11 py-2 border-t-4 border-blue-900'>
-                    <img src={img1} alt="zappos" srcSet="" className='h-6 inline align-middle text-white '/>
+                <div className='bg-sky-600 hover:bg-sky-500 px-11 border-t-2 border-blue-900'>
+                    <img src={img1} alt="zappos" srcSet="" className='h-4 inline align-middle text-white '/>
                 </div>
-                <div  className='px-10 py-2 border-r-2'>
+                <div  className='px-10  border-r-2'>
                 <img src={img2} alt="vrsnl" srcSet="" className='h-3 inline align-middle'/>
                 </div>
             </div>
