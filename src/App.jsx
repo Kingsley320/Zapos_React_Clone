969ebe5a79076bb31245842ba9a654e29d71d8aa
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-07-01 19:32:03 +0100

    added new routes to project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Landing from './pages/Landing';
 
 
 import './App.css'
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Cart from './pages/cart';
+import Checkout from './pages/Checkout';
+import Shop from './pages/Shop';
+import SingleProduct from './pages/SIngleProduct';
 
 function App() {
 
@@ -11,6 +17,12 @@ function App() {
       <BrowserRouter >
        <Routes>
         <Route path="/" element={<Landing />}/>
+        <Route path="/sign-up" element={<SignUp />}/>
+        <Route path="/sign-in" element={<SignIn />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/checkout" element={<Checkout />}/>
+        <Route path="/shop" element={<Shop />}/>
+        <Route path="/single-product" element={<SingleProduct />}/>
        </Routes>
       </BrowserRouter>
     </>
