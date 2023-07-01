97fa90007efbba3dcc23ded5af74114c5c906839
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-07-01 19:33:20 +0100

    pending git tracked files

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        '1': '1px',
+        '3': '3px',
+      },
+      height: {
+        '18': '68px',
+      },
+      spacing: {
+        '85': '21.5rem',
+        '88': '22.5rem'
+      },
+      fontFamily: {
+        'Arial': ['Helvetica', 'sans-serif',]
+      },
+    },
   },
   plugins: [],
 }
