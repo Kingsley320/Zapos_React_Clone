2df1ebded733fb816167a58c33cc4edb51916cfe
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-07-04 23:50:02 +0100

    tried using headless UI to create a mega menu, no avail yet

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -9,7 +9,7 @@ function Categories() {
     return (
         <>
             <div className='flex px-2 mx-auto  bg-gray-100/75 overflow-hidden h-12 relative'>
-                <div className=' flex my-auto  h-14 font-verdana'>
+                <div className=' flex my-auto align-middle h-14 font-verdana'>
                     <ul className="flex  gap-3 text-md font-semibold ml-5 my-auto  ">
                         <li className="flex theblue"><button className=" mb-3 flex">New <BsFillCaretDownFill className="text-gray-300 text-sm my-auto p-0.5" /></button>  </li>
                         <li className="flex theblue"><button className=" my-auto mb-3 flex">Women <BsFillCaretDownFill className="text-gray-300 text-sm my-auto p-0.5" /></button>  </li>
@@ -17,12 +17,12 @@ function Categories() {
                             <Menu>
                                 <Menu.Button className="flex gap-1 text-blue-custom ">More  <BsFillCaretDownFill className="text-gray-300 text-sm my-auto p-0.5" /></Menu.Button>
                                 <Menu.Items className="absolute top-4 bg-white border-2 border-gray">
-                                    <Menu.Item>
+                                    <Menu.Item as="a" href={'/shop'} className="z-10">
                                         {({ active }) => (
-                                            <div className="grid grid-cols-6 w-[90vw] h-900px">
+                                            <div className="grid grid-cols-6 w-[90vw] h-900px ">
                                                 <div className="text-sky-800">
                                                     <ul>
-                                                        <li><Link to={'/shop'} element={<Shop/>}>Shoes</Link></li>
+                                                        <li>Shoes</li>
                                                         <li>Sneakers & Athletic</li>
                                                         <li>Sandals</li>
                                                         <li>Running Shoes</li>
